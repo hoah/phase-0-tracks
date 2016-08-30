6719c0fcfe9915b9486b97a0474e413c1654a0d9
Hoa Huynh <hoah@hotmail.com>
2016-08-30 09:44:37 -0500

    5.2 start of interior design program

---
+++ ruby/hashes.rb
@@ -0,0 +1,6 @@
+# Prompt the designer/user for all of this information.
+# Convert any user input to the appropriate data type.
+# Print the hash back out to the screen when the designer has answered all of the questions.
+# Give the user the opportunity to update a key
+# Print the latest version of the hash, and exit the program.
+
