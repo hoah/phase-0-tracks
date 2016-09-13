c0f04b2eb8eb492122bd4b0e75bdaca90d5e33c9
Hoa Huynh <hoah@hotmail.com>
2016-09-13 14:28:38 -0500

    7.2 pairing: Release 0

---
+++ js/data_structures.js
@@ -0,0 +1,9 @@
+var colors = ["blue", "green", "red", "yellow"];
+var names = ["Ed", "Bob", "Joe", "Ann"];
+
+colors.push("grey");
+names.push("Fluffy");
+
+
+console.log(colors);
+console.log(names);
