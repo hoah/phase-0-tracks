93d87dc89a5bf9cc48d7f07c012672073c623c75
Hoa Huynh <hoah@hotmail.com>
2016-09-14 17:58:50 -0500

    Add release 0: Longest phrase

--- js/algos.js
+++ js/algos.js
@@ -1,3 +1,18 @@
-var phrase = ["long phrase","longest phrase","longer phrase"];
+var phrase = ["long phrase", "longest phrase", "longer phrase"];
+var anotherPhrase = ["today", "tomorrow", "next month"];
 
-function()
+function longestPhrase(phrase) {
+  var longPhrase_index = 0;
+
+  for(var i = 0; i < phrase.length; i++) {
+    if (phrase[longPhrase_index].length < phrase[i].length) {
+      longPhrase_index = i;
+    }
+  }
+
+  console.log(phrase[longPhrase_index]);
+}
+
+
+longestPhrase(phrase);
+longestPhrase(anotherPhrase);
