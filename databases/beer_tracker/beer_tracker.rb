f62e62a081f0ad4fe4851c69a749a2ad51e3336d
Hoa Huynh <hoah@hotmail.com>
2016-09-25 09:54:21 -0500

    Create beer tracker database and tested

--- databases/beer_tracker/beer_tracker.rb
+++ databases/beer_tracker/beer_tracker.rb
@@ -10,8 +10,11 @@ db = SQLite3::Database.new("beer_tracker.db")
 create_table_cmd = <<-SQL
   CREATE TABLE IF NOT EXISTS beer_tracker(
     id INTEGER PRIMARY KEY,
-    name VARCHAR(255),
-    age INT
+    beer_name VARCHAR(255),
+    brewed_in VARCHAR(255),
+    served_from VARCHAR(255),
+    star_rating INTEGER,
+    last_drank DATE
   )
 SQL
 
@@ -19,11 +22,27 @@ SQL
 db.execute(create_table_cmd)
 
 
-def create_kitten(db, name, age)
-  db.execute("INSERT INTO kittens (name, age) VALUES (?, ?)", [name, age])
+def create_beer_tracker(db, beer_name, brewed_in, served_from, star_rating, last_drank)
+  db.execute("INSERT INTO beer_tracker (beer_name, brewed_in, served_from, star_rating, last_drank)
+    VALUES (?, ?, ?, ?, ?)",
+    [beer_name, brewed_in, served_from, star_rating, last_drank])
 end
 
-1000.times do
-  create_kitten(db, Faker::Name.name, 0)
+
+
+
+# test driver code
+
+test_array = [
+  ["Sam Adams", "domestic", "bottle", 3, "2016-08-25"],
+  ["Fat tire", "domestic", "bottle", 4, "2016-09-24"],
+  ["512 pecan porter", "domestic", "draft", 5, "2016-09-25"]
+]
+
+test_array.each_index do |i|
+  create_beer_tracker(db, test_array[i][0], test_array[i][1], test_array[i][2], test_array[i][3], test_array[i][4])
 end
+
+
+# driver code
 
